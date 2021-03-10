60c74e2a9b13b8142933b79cf6062bab20b06bc6
HSU-CHIA-WEN <kuso.dir@gmail.com>
2021-03-10 16:32:31 +0800

    更新 omnifood 網頁位置

---
+++ demos/omnifood/vendors/resources/js/script.js
@@ -0,0 +1,96 @@
+/* /For the sticky navigation */
+$(document).ready(function () {
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    { offset: "60px" }
+  );
+
+  /* Scroll on buttons */
+  $(".js--scroll-to-plans").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
+      1000
+    );
+  });
+  $(".js--scroll-to-start").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
+      1000
+    );
+  });
+
+  /* Navigation scroll */
+  $(function () {
+    $("a[href*='#']:not([href='#'])").click(function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+  });
+
+  /* Animations on scroll */
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animate__animated animate__fadeIn");
+    },
+    { offset: "50%" }
+  );
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animate__animated animate__fadeInUp");
+    },
+    { offset: "50%" }
+  );
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animate__animated animate__fadeIn");
+    },
+    { offset: "50%" }
+  );
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass("animate__animated animate__pulse");
+    },
+    { offset: "50%" }
+  );
+
+  /* Mobile nav */
+  $(".js--nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+    var icon = $(".js--nav-icon ion-icon");
+
+    nav.slideToggle(200);
+
+    if (icon.attr("name") == "menu") {
+      icon.attr("name", "close");
+    } else {
+      icon.attr("name", "menu");
+    }
+  });
+});
+
+{
+  /* <ion-icon name="close"></ion-icon> */
+}
